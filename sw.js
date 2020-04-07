a00fc18765f3a745f0f61e3a5aaa9a4a4f16b548
王寒 <wang187335@163.com>
2020-04-07 18:45:34 +0800

    修改评价

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-var cacheName = 'hello-pwa3';
+var version = 'hello-pwa3';
 
+// Serverice Worker 安装成功后触发该事件
 self.addEventListener('install', event => {
     console.log('event:',event);
+    // sw.js 有更新，立即生效
     event.waitUntil(
-        caches.open(cacheName)
+        caches.open(version)
             .then(cache => cache.addAll(
                 [
                     '/',  // 这个一定要包含整个目录，不然无法离线浏览
@@ -13,21 +15,67 @@ self.addEventListener('install', event => {
                     './login.html',
                     './register.html',
                     './css/index.css',
-                    './css/main.css'
+                    './main.css',
+                    './socket.io.js'
                 ]
             )).then(() => self.skipWaiting())
     );
 });
 
-self.addEventListener('fetch', function (event) {
-    console.log('event',event);
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
+// sw.js 有更新时触发该事件
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        Promise.all([
+            // 更新客户端
+            self.clients.claim(),
+
+            // 删除旧版本的缓存对象
+            caches.keys().then(function (cacheList) {
+                return Promise.all(
+                    cacheList.map(function (cacheName) {
+                        if (cacheName !== version) {
+                            return caches.delete(cacheName);
+                        }
+                    })
+                );
             })
+        ])
+    );
+});
+
+// 网页发送请求触发该事件
+self.addEventListener('fetch', function (event) {
+    // 打印请求资源网址
+    console.log('url is', event.request.url);
+    event.respondWith(
+        // caches 是全局变量，它就是缓存对象
+        // 这一步是判断缓存中是否有该资源
+        caches.match(event.request).then(function (cacheRes) {
+
+            // cacheRes 不为空，缓存中有该资源，直接返回给浏览器
+            // 省去一次 http 请求
+            if (cacheRes) {
+                return cacheRes;
+            }
+            // cacheRes 为空，表示缓存中没有该请求
+            // 把原始请求拷过来
+            var request = event.request.clone();
+
+            // fetch 是浏览器自带的请求库，往服务端发送请求
+            return fetch(request).then(function (httpRes) {
+                // 请求失败了，直接返回失败的结果就好了。。
+                if (!httpRes || httpRes.status !== 200) {
+                    return httpRes;
+                }
+
+                // 请求成功的话，将响应添加到缓存中
+                // 下一次请求相同资源时直接从缓存中拿资源，就不用再发请求了
+                // var responseClone = httpRes.clone();
+                // caches.open(version).then(function (cache) {
+                //     cache.put(event.request, responseClone);
+                // });
+                return httpRes;
+            });
+        })
     );
 });
